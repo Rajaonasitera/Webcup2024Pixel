af432d020f98f3dd9f9760df6490edb3fcce4198
Daniax18 <daniamidera@gmail.com>
2024-05-05 00:19:41 +0300

    delete views

--- application/views/bo/main.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $this->load->view("bo/content/link");	
-    $this->load->view("bo/content/menu");	
-    $this->load->view("bo/content/header");	
-    $this->load->view($content);
-    $this->load->view("bo/content/footer");	
